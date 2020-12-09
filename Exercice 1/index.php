2854c0099ab8b06deae0cc5097dc87c094317c5b
Nivash <a@a.a>
2020-12-09 11:54:08 +0100

    Fin de l'exercice 1

---
+++ Exercice 1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Exercice 1 </h1>
+
+    <?php
+    echo "<ul>
+            <li>Pomme</li>
+            <li>Abricot</li>
+            <li>Fraise</li>
+            <li>Raisins</li>
+            <li>Kiwi</li>
+            </ul>";
+    ?>
+
+</body>
+</html>
