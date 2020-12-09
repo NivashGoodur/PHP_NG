d0f709e988e1a8b4da22ff5984441d0b3d219c4f
Nivash <a@a.a>
2020-12-09 14:15:03 +0100

    Fin de l'exercice 2

---
+++ Exercice 2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php
+    $color = 'red';
+    echo '<body style ="background-color :'.$color.'">';
+    ?>
+</head>
+<body>
+
+    <?php
+
+    $first_name = 'Nivash';
+    echo('Salut '.$first_name.'!');
+
+    ?>
+
+</body>
+</html>
