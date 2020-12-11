768a38258ca409ddbb22f861abe3426efdf52c75
Nivash <a@a.a>
2020-12-11 11:41:30 +0100

    correction ex9

--- Exercice 9/index.php
+++ Exercice 9/index.php
@@ -1,9 +1,11 @@
 <?php
 
 //Exercice 1 :
-function print_rv2 ($array_pre){
+
+//Fonction permettant d'afficher le contenu d'une variable avec un print_r entouré d'une balise html <pre>
+function print_rv2 ($elementsToDisplay){
     echo '<pre>';
-    print_r($array_pre);
+    print_r($elementsToDisplay);
     echo'</pre>';
 
 }
@@ -12,16 +14,33 @@ function print_rv2 ($array_pre){
 $fruits = ['Pomme', 'Poire', 'Cerise', 'Kiwi'];
 //$test = ['test1', 'test2', 'test3', 'test4'];
 
-print_rv2($fruits);
-
-
 
 //Exercice 2 :
 
-function getTTCPrice($price_ht, $tva){
-    return $price_ht/100*$tva +$price_ht;
+//Fonction qui retourne un prix initial additionné avec une taxe précisé en second paramètre (20% par défaut)
+function getTTCPrice($htPrice, $tax = 20){
+    return $htPrice *(($tax / 100)+1) ;
 }
 
-echo getTTCPrice(100,10);
-
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    //Affichage de l'array des fruits avec la nouvelle fonction
+    print_rv2($fruits);
+
+    //Affichage du prix TTC de 19€ avec (tva = 20 par défaut)
+    echo getTTCPrice(19);
+    ?>
+</body>
+</html>
